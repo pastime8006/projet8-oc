948aabf46085ab171f9c6a332e9e24e1131ab281
paweb <pastime@posteo.net>
2026-09-04 11:39:34 +0200

    ajout des addEvenlistener et inversion du sens de defilement

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,51 +1,6 @@
 
 
-// let slides = [
-//   {
-//     image: "assets/images/slideshow/slide1.jpg",
-//     tagline: "Impressions tous formats <span>en boutique et en ligne</span>",
-//   },
-//   {
-//     image: "assets/images/slideshow/slide2.jpg",
-//     tagline: "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-//   },
-//   {
-//     image: "assets/images/slideshow/slide3.jpg",
-//     tagline: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-//   },
-//   {
-//     image: "assets/images/slideshow/slide4.png",
-//     tagline: "Autocollants <span>avec découpe laser sur mesure</span>",
-//   },
-// ];
-
-// let imageActive = 0;
-
-// let image = document.querySelector("#slider .slider-img");
-// let tagline = document.querySelector("#slider .tagline");
-// updateImage();
-
-// let arrowRight = document.querySelector("#slider .arrow-right");
-// arrowRight.onclick = function (e) {
-//   imageActive++;
-//   if (imageActive > slides.length - 1) imageActive = 0;
-//   updateImage();
-// };
-
-// let arrowLeft = document.querySelector("#slider .arrow-left");
-// arrowLeft.onclick = function (e) {
-//   imageActive--;
-//   if (imageActive < 0) imageActive = slides.length - 1;
-//   updateImage();
-// };
-
-// function updateImage() {
-//   image.setAttribute("src", slides[imageActive].image);
-//   tagline.innerHTML = slides[imageActive].tagline;
-// }
-
-
-  const slides = [
+const slides = [
     {
       image: "assets/images/slideshow/slide1.jpg",
       tagline: "Impressions tous formats <span>en boutique et en ligne</span>",
@@ -64,34 +19,28 @@
     },
   ];
 
-  let currentSlide = 0;
+  let imageActive = 0;
 
-  const image = document.querySelector("#slider .slider-img");
-  const tagline = document.querySelector("#slider .tagline");
+  let image = document.querySelector("#slider .slider-img");
+   tagline = document.querySelector("#slider .tagline");
+
+  updateImage();
+
   const arrowRight = document.querySelector("#slider .arrow-right");
+  arrowRight.addEventListener('click', () =>  {
+    imageActive++;
+    if (imageActive > slides.length - 1) imageActive = 0;
+    updateImage();
+  });
+
   const arrowLeft = document.querySelector("#slider .arrow-left");
+  arrowLeft.addEventListener('click', () => {
+    imageActive--;
+    if (imageActive < 0) imageActive = slides.length - 1;
+    updateImage();
+  });
 
-  function updateSlide() {
-    const slide = slides[currentSlide];
-
-    image.src = slide.image;
-    tagline.innerHTML = slide.tagline;
+  function updateImage() {
+    image.setAttribute("src", slides[imageActive].image);
+    tagline.innerHTML = slides[imageActive].tagline;
   }
-
-  function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    updateSlide();
-  }
-
-  function previousSlide() {
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    updateSlide();
-  }
-
-  arrowRight.addEventListener("click", nextSlide);
-  arrowLeft.addEventListener("click", previousSlide);
-
-  updateSlide();
-
-
-   
